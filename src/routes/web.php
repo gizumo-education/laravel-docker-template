a3d595ab74a734d46ee7c75a0483830d6569bf50
Nakahota <nakay5251@gmail.com>
2023-11-02 13:15:03 +0900

    feat:ToDo詳細表示機能を追加。

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,5 @@ Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 Route::post('/todo','TodoController@store')->name('todo.store');
 
 Route::get('/todo', 'TodoController@index')->name('todo.index');
+
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
