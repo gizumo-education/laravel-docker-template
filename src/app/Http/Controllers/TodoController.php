e5a4b639667af2c843cde50bb3fa715e24693fb7
ippei1313 <ippei1313@gmail.com>
2023-11-02 17:45:59 +0900

    bladeファイルの分割

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+   private $todo;
+
+  public function __construct(Todo $todo)
+   {
+      $this->todo = $todo;
+   }
+      
   public function create()
    {
    return view('todo.create');
    }
   public function store(Request $request)
    {
-      dd($request->all());
+      
+      $inputs = $request->all();
+      $this->todo->fill($inputs);
+      $this->todo->save();
    }
 
 }
