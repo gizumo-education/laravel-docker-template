72262b33b0de76edd70b141706a0b197ae0388e2
ippei1313 <ippei1313@gmail.com>
2023-11-15 15:38:57 +0900

    add:削除機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -9,25 +9,24 @@ class TodoController extends Controller
 {
    private $todo;
 
-  public function __construct(Todo $todo)
+   public function __construct(Todo $todo)//
    {
       $this->todo = $todo;
    }
 
    public function index()
-    {
-        $todos = $this->todo->all();
-        return view('todo.index', ['todos' => $todos]);
-    }
-      
-  public function create()
    {
-   return view('todo.create');
+      $todos = $this->todo->all();
+      return view('todo.index', ['todos' => $todos]);
+   }
+      
+   public function create()
+   {
+      return view('todo.create');
    }
 
   public function store(TodoRequest $request)
    {
-      
       $inputs = $request->all();
       $this->todo->fill($inputs);
       $this->todo->save();
@@ -35,24 +34,31 @@ class TodoController extends Controller
    }
 
    public function show($id)
-    {
+   {
       $todo = $this->todo->find($id);
       return view('todo.show', ['todo' => $todo]);
-    }
+   }
 
-    public function edit($id)
-    {
-        $todo = $this->todo->find($id);
-        return view('todo.edit', ['todo' => $todo]);
-    }
+   public function edit($id)
+   {
+      $todo = $this->todo->find($id);
+      return view('todo.edit', ['todo' => $todo]);
+   }
 
-    public function update(TodoRequest $request, $id)
-    {
+   public function update(TodoRequest $request, $id)
+   {
       $inputs = $request->all();
       $todo = $this->todo->find($id);
       $todo->fill($inputs);
       $todo->save();
       return redirect()->route('todo.show', $todo->id);
+   }
+
+   public function delete($id)
+    {
+      $todo = $this->todo->find($id);
+      $todo->delete();
+      return redirect()->route('todo.index');
     }
 }
 
