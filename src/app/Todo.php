7447d3e3ad178e88299f3b963f3d4665dc01c3f1
Nakahota <nakay5251@gmail.com>
2023-11-30 14:27:37 +0900

    'feat:DELETE機能を物理削除から論理削除に変更。'

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,10 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Todo extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'todos';
+
     protected $fillable = [
         'content'
     ];
