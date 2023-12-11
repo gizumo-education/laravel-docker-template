f057e69c3d66162348b1df043efb49bb137fdf51
akks0916 <akkswork1609@gmail.com>
2023-12-12 01:26:44 +0900

    feat:Todo完了機能を実装

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -11,15 +11,18 @@
       </div>
       <div class="list-group list-group-flush">
       @foreach ($hoge as $todo)
-        <div class="d-flex">
+      <div class="d-flex">
+          <form action="{{ route('todo.complete', $todo->id) }}" class="px-3 my-auto todo-status-form">
+            <input type="checkbox" class="form-control todo-status-button" name="id" value={{ $todo->content }} @if ($todo->is_completed) checked @endif>
+          </form>
           <a href="{{ route('todo.show', $todo->id) }}" class="list-group-item list-group-item-action">
             {{ $todo->content }}
-          
           </a>
-        </div>
+      </div>
       @endforeach
       </div>
     </div>
   </div>
 </div>
+<script src="{{ asset('js/index.js') }}"></script>
 @endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -20,3 +20,4 @@ Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
 Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
 Route::delete('/todo/{id}', 'TodoController@delete')->name('todo.delete');
+Route::post('/todo/{id}/complete', 'TodoController@complete')->name('todo.complete');
