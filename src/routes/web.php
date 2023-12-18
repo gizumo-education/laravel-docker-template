c2420404d11b761fbeb0a301ea0570beb1bd728a
nnnasan <79150609+nnnasan@users.noreply.github.com>
2023-12-18 18:31:16 +0900

    add: Todo一覧画面ファイルの新規追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -28,3 +28,5 @@
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+
+Route::get('/todo', 'TodoController@index')->name('todo.index');
