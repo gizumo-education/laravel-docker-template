6176ddaba62fca68c600827afc9011b6808b08a8
nnnasan <79150609+nnnasan@users.noreply.github.com>
2023-12-18 12:36:38 +0900

    add: todosテーブル用のマイグレーションファイル作成。テストデータ挿入用のseederファイルを作成。

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // 以下追加したもの
+        DB::table('todos')->insert([
+            [
+                'content' => '開発環境を構築する',
+                'is_completed' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'content' => 'Laravelをインストールする',
+                'is_completed' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
