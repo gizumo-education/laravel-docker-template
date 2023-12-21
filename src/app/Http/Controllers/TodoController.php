bde2e8600056eb580998a260b6968d01c85abe7b
ei1017 <tsurugaxotaniy1600@gmail.com>
2023-12-21 16:48:31 +0900

    CREATE＆UPDATE：バリデーション機能実装、条件に当てはまるときにエラーメッセージを表示させる機能を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use App\Todo;
-use Illuminate\Http\Request;
+
+// use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
@@ -25,7 +27,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
         $this->todo->fill($inputs);
@@ -45,7 +47,7 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
         $todo = $this->todo->find($id);
