e933b6dbcbc80946c6a336a22aca90f5c3a89b41
Misato M <misatom@MisatonoMacBook-Air.local>
2023-12-25 14:49:29 +0900

    add: todosテーブル用のマイグレーションファイル作成。テストデータ挿入用のseederファイルを作成。

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -8,9 +8,11 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      *
      * @return void
-     */
+     */ 
+    \
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);削除
+        $this->call(TodosTableSeeder::class);
     }
 }
---
+++ src/database/seeds/TodosTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TodosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //以下追加
+        DB::table('todos')->insert([
+            [
+                'content' => '開発環境を構築する',
+                'is_completed' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'content' => 'Laravelをインストールする',
+                'is_completed' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
