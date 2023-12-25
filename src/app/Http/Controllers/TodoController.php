e40dfc53af69674d28d08cbaf10ff5d2ed41be7e
Misato M <misatom@MisatonoMacBook-Air.local>
2023-12-25 17:20:12 +0900

    Controllerの作成。routes.web.phpを更新し、登録内容のルートを定義。formタグを修正。Controllersファイルを修正し、リクエストされた値を表示。

---
+++ src/app/Http/Controllers/TodoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    //追加
+    public function create()
+    {
+        return view('todo.create');
+    }
+     // 追加
+     public function store(Request $request)
+     {
+         dd($request->all());
+     }
+    
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,6 +19,12 @@
 //     echo 'Hello World!';
 // });
 
-Route::get('/todo/create', function () {
-    return view('todo.create');
-})->name('todo.create');
+// Route::get('/todo/create', function () {
+//     return view('todo.create');
+// })->name('todo.create');
+
+// 修正
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
+// 追加
+Route::post('/todo', 'TodoController@store')->name('todo.store');
+
