da87e6a72c3a8a53f5545e4ff0edfae86f88db83
n-yohei1996 <nishi.youhei8810@gmail.com>
2024-01-22 11:06:28 +0900

    add:Todo完了機能の実装

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+    use SoftDeletes;
     protected $table = 'todos';
     protected $fillable = [
         'content'
