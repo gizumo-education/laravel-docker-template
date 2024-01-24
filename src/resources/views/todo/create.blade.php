c0f231d4eb32e74b3dd0e253c3ee19056b512db4
Fish221-deku166 <satoayane166@gmail.com>
2024-01-24 23:38:38 +0900

    feat: CSRF処理の追加,リクエスルートの追加

--- src/resources/views/todo/create.blade.php
+++ src/resources/views/todo/create.blade.php
@@ -31,19 +31,20 @@
             <div class="card">
               <div class="card-header">ToDo作成</div>
               <div class="card-body">
-                <form method="" action="">
-                  <div class="form-group row">
-                    <label for="name" class="col-md-4 col-form-label text-md-right">ToDo入力</label>
-                    <div class="col-md-6">
-                      <input type="text" class="form-control" name="content" value="">
-                    </div>
+              <form method="POST" action="{{ route('todo.store') }}">
+                @csrf
+                <div class="form-group row">
+                  <label for="name" class="col-md-4 col-form-label text-md-right">ToDo入力</label>
+                  <div class="col-md-6">
+                    <input type="text" class="form-control" name="content" value="">
                   </div>
-                  <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                      <button type="submit" class="btn btn-primary">作成</button>
-                    </div>
+                </div>
+                <div class="form-group row mb-0">
+                  <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary">作成</button>
                   </div>
-                </form>
+                </div>
+              </form>
               </div>
             </div>
           </div>
