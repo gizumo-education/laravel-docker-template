81774a1f09578970e57cc6986a23cf6675c75704
sakurai-tech <s.y.asdlkj@gmail.com>
2024-01-31 18:46:09 +0900

    TodoのModel作成。ControllerからModelの呼び出し。

---
+++ src/app/Todo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    //
+    protected $table = 'todos';
+
+    protected $fillable = [
+        'content'
+    ];
+}
