76f940134c6f0902770a2417b069921927764995
Fish221-deku166 <satoayane166@gmail.com>
2024-02-04 21:46:00 +0900

    add:softdelete_tableマイグレーション追加

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
 
+    use SoftDeletes; 
+    
     protected $table = 'todos';
 
     protected $fillable = [
