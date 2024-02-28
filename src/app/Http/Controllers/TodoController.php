4ad38b1f9901a3a8e5564469263b985cf54bd0fd
鈴木真優 <suzukimahiro@suzukimahironoMacBook-Air.local>
2024-02-28 10:26:25 +0900

    feat:空欄送信と文字数上限に対してエラーメッセージを表示する機能を追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use App\Todo;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 // use resources\view;
 
 class TodoController extends Controller
@@ -26,7 +27,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
         $this->todo->fill($inputs);
@@ -46,7 +47,7 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
         $todo = $this->todo->find($id);
