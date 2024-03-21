39d7c165e1808707093cb3fcf1447a66e5f7d5fc
JinguuTukasa <desusousu0@gmail.com>
2024-03-21 13:03:43 +0900

    add:バリデーションの実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use App\Todo;
-use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
 
 class TodoController extends Controller
 {
@@ -19,7 +20,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
         $this->todo->fill($inputs);
@@ -46,7 +47,7 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
         $todo = $this->todo->find($id);
---
+++ src/app/Http/Requests/TodoRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TodoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // ここがfalseのままだと全てのリクエストを受け付けなくなってしまうので注意
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'content' => 'required|max:255',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'content.required' => 'ToDoが入力されていません。',
+            'content.max' => 'ToDoは :max 文字以内で入力してください。',
+        ];
+    }
+}
