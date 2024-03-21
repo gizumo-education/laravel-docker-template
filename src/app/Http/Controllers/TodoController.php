da92bd4fd87114c7b222a4e38e626a1bbfdbeb01
JinguuTukasa <desusousu0@gmail.com>
2024-03-21 17:06:36 +0900

    add:最終調整

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Http\Requests\TodoRequest;
@@ -10,43 +11,51 @@ class TodoController extends Controller
 {
     private $todo;
 
+    // TodoControllerのインスタンスを作成するときに、Todoモデルを受け取り、プロパティに設定する
+    //モデルのひな型を作成
     public function __construct(Todo $todo)
     {
         $this->todo = $todo;
     }
 
+    // Todo作成用のフォームを表示する
     public function create()
     {
         return view('todo.create');
     }
 
+    // フォームから送信されたTodoを保存する
     public function store(TodoRequest $request)
     {
         $inputs = $request->all();
-        $this->todo->fill($inputs);
-        $this->todo->save();
+        $this->todo->fill($inputs); //fillメソッドですべての入力データをモデルに設定（ただ更新できるのはtodo.phpでホワイトリストにしているもののみ）
+        $this->todo->save(); //モデルの状態をデータベースに保存している　update関数
         return redirect()->route('todo.index');
     }
 
+    // すべてのTodoを取得して、一覧を表示する
     public function index()
     {
-        $todos = $this->todo->all();
-        return view('todo.index', ['todos' => $todos]);
-        dd($todos);
+        $todos = $this->todo->all(); //Todoのホワイトリストに設定したすべてを取得　Select文　Collectionクラス
+        return view('todo.index', ['todos' => $todos]); //第一引数は対象のBladeファイル　第二引数はそのBladeファイルで呼び出せる変数
+        //dd($todos); 
     }
 
+    // 特定のTodoを表示する
     public function show($id)
     {
         $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
 
+    // 特定のTodoを編集するためのフォームを表示する
     public function edit($id)
     {
         $todo = $this->todo->find($id);
         return view('todo.edit', ['todo' => $todo]);
     }
 
+    // フォームから送信されたTodoを更新する
     public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
@@ -56,5 +65,6 @@ class TodoController extends Controller
         return redirect()->route('todo.show', $todo->id);
     }
 }
+
 
 //なぜfill()を利用したのか。そのメリットをご自身で調べてみてください（レビュー時に聞きます）。
--- src/app/Todo.php
+++ src/app/Todo.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
+    // モデルが使用するデータベーステーブルの名前を指定します
     protected $table = 'todos';
 
+    // モデルの属性をホワイトリストで設定します。ここで指定された属性のみ、fillメソッドで一括代入できます
     protected $fillable = [
         'content'
     ];
