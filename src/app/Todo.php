dde5ec282e1157cef02cb569258f9570b3010c5d
JinguuTukasa <desusousu0@gmail.com>
2024-03-28 13:07:35 +0900

    add:論理削除の実装２

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+
+    use SoftDeletes;
+
     // モデルが使用するデータベーステーブルの名前を指定します
     protected $table = 'todos';
 
