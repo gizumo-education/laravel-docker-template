d3dd4c1ddd8db2accb00996ef6e28c62eb9db457
shojitayo6723 <shirokuma7233@gmail.com>
2024-04-04 08:13:18 +0900

    add:ToDoの一覧画面の共通部分の親テンプレートを作成する為にbese.blade.phpファイルを作成。

--- src/resources/views/todo/create.blade.php
+++ src/resources/views/todo/create.blade.php
@@ -12,20 +12,11 @@
 </head>
 <body>
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+
       <div class="container">
-        <a class="navbar-brand" href="/todo">ToDo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto"></ul>
-          <ul class="navbar-nav ml-auto"></ul>
-        </div>
-      </div>
-    </nav>
-    <main class="py-4">
-      <div class="container">
+      @extends('layouts.base')
+
+      @section('content')
         <div class="row justify-content-center">
           <div class="col-md-8">
             <div class="card">
@@ -49,6 +40,7 @@
             </div>
           </div>
         </div>
+        @endsection
       </div>
     </main>
   </div>
