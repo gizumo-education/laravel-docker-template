3e818c2e7101bd2a44dc81d1d843575d3aedb549
shojitayo6723 <shirokuma7233@gmail.com>
2024-04-04 09:50:00 +0900

    feat:バリテーション機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use App\Todo;
-use Illuminate\Http\Request;
+
 
 class TodoController extends Controller
 {
@@ -26,7 +27,7 @@ class TodoController extends Controller
     }
 
     
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
         $this->todo->fill($inputs);
@@ -47,7 +48,7 @@ class TodoController extends Controller
     }
 
 
-    public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
         $todo = $this->todo->find($id);
