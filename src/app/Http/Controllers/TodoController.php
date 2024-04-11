c8a15b6f347baddea25c73c3af2d9802872098c2
Oonuki Yuuma <onuki1009@gmail.com>
2024-04-11 10:49:13 +0900

    add:編集機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -39,4 +39,18 @@ class TodoController extends Controller
         return view('todo.show', ['todo' => $todo]);
     }
 
+    public function edit($id)
+    {
+        $todo = $this->todo->find($id);
+        return view('todo.edit', ['todo' => $todo]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        $todo = $this->todo->find($id);
+        $todo->fill($inputs);
+        $todo->save();
+        return redirect()->route('todo.show', $todo->id);
+    }
 }
