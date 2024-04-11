4352678aa3616f62aed150d6ade01036c3ec4882
Oonuki Yuuma <onuki1009@gmail.com>
2024-04-11 10:20:58 +0900

    add:一覧表示機能の実装

---
+++ src/resources/views/todo/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.base')
+@section('content')
+<div class="row justify-content-center">
+  <div class="col-md-8">
+    <p class="text-left">
+      <a class="btn btn-success" href="/todo/create">ToDoを追加</a>
+    </p>
+    <div class="card">
+      <div class="card-header">
+        ToDo一覧
+      </div>
+      <div class="list-group list-group-flush">
+      @foreach ($todos as $todo)
+        <div class="d-flex">
+          <a href="#" class="list-group-item list-group-item-action">
+            {{ $todo->content }}
+          </a>
+        </div>
+      @endforeach
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
