1a98db0813ef7313e96d677f873d14ab8f769d42
丸山智也 <maruyamatomoya@MacBook.local>
2024-05-30 12:31:38 +0900

    feat:TodoControllerのindexメソッドに処理を移譲/add:一覧表示画面の追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todo', function () {
-    echo 'Hello World!';
-});
+// Route::get('/todo', function () {
+//     echo 'Hello World!';
+// });
+
+Route::get('/todo', 'TodoController@index');
