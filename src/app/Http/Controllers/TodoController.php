13b998f5274b736f17ee89fb0b7761d5aeee889f
丸山智也 <maruyamatomoya@MacBook.local>
2024-05-30 12:54:09 +0900

    feat:DBのレコードを全権取得する処理の追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -11,6 +11,6 @@ class TodoController extends Controller
         $todo = new Todo();
         $todoList = $todo->all();
 
-        return view('todo.index');
+        return view('todo.index', ['todoList' => $todoList]);
     }
 }
