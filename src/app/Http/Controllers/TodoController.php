bc151f6df3460c0e037bb94075b2f5c69e89327f
丸山智也 <maruyamatomoya@MacBook.local>
2024-05-30 12:48:14 +0900

    feat:DBのレコードを全権取得する処理の追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todoList = $todo->all();
+
         return view('todo.index');
     }
 }
