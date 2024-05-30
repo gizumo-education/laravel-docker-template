fbebab3c1a2943348910a673d291850b838a1394
丸山智也 <maruyamatomoya@MacBook.local>
2024-05-30 13:35:47 +0900

    feat:「ToDoを追加」ボタンを一覧画面に表示&新規作成画面の表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -13,4 +13,9 @@ class TodoController extends Controller
 
         return view('todo.index', ['todoList' => $todoList]);
     }
+
+    public function create()
+    {
+        return view('todo.create');
+    }
 }
--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -31,7 +31,7 @@
         <div class="row justify-content-center">
           <div class="col-md-8">
             <p class="text-left">
-              <a class="btn btn-success" href="">ToDoを追加</a>
+              <a class="btn btn-success" href="http://localhost:8080/todo/create">ToDoを追加</a>
             </p>
             <div class="card">
               <div class="card-header">
