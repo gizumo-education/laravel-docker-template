c66340ccf7d9eaa5c6abbb2eb701e885ad9c2ec8
wdjws436 <wdjws436@gmail.com>
2024-06-02 22:15:50 +0900

    add:ToDo論理削除機能の実装

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+    use SoftDeletes;
     // 追加
     protected $table = 'todos';
     // 追加
