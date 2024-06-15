c1adfe61b409790988dfd43569b35f01629dbb33
丸山智也 <maruyamatomoya@MacBook.local>
2024-06-15 15:11:42 +0900

    feat:ToDo削除機能の実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -33,3 +33,5 @@ Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
 
 Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+
+Route::delete('/todo/{id}/show', 'TodoController@delete')->name('todo.delete');
