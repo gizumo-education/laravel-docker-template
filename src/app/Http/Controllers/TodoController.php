5716864b4ff00da95103728d48026a19cf16587c
takamasa hirano <pingyexiaoshang@gmail.com>
2024-06-20 13:04:24 +0900

    add:DBとマッピングのためTodo.phpを作成、TodoController.phpにtodoテーブルから情報を一覧化するコードを追記

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
+
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todoList = $todo->all();
+
         return view('todo.index');
     }
 }
