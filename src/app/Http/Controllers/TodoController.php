722347dc85ab95e08a647fdf8f8832d6b0da85b0
takamasa hirano <pingyexiaoshang@gmail.com>
2024-06-20 17:11:58 +0900

    feat:TodoController.phpにstore(request)関数を記載し、DBに接続データ保存機能追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,16 +8,30 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    public function create()
-    {
-        return view('todo.create');
-    }
-
     public function index()
     {
         $todo = new Todo();
         $todoList = $todo->all();
 
         return view('todo.index',['todoList' => $todoList]);
+    }
+
+    public function create()
+    {
+        return view('todo.create');
+    }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content');
+        
+        // 1.todosテーブルの1レコードを表すtodoクラスをインスタンス化
+        $todo = new Todo();
+
+        // 2.Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->content = $content;
+
+        //3.Todoインスタンスの->save()を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->save();
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -16,5 +16,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todo', 'TodoController@index');
+Route::get('/todo', 'TodoController@index')->name('todo.index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
+Route::post('/todo', 'TodoController@store')->name('todo.store');
+
+
