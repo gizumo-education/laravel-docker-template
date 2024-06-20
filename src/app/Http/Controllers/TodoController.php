b75165aa87f9d21e12f7e834319e773f6dbc564c
takamasa hirano <pingyexiaoshang@gmail.com>
2024-06-20 14:29:48 +0900

    feat:TodoController.phpにview関数を記入し取得データを渡す

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -13,6 +13,6 @@ class TodoController extends Controller
         $todo = new Todo();
         $todoList = $todo->all();
 
-        return view('todo.index');
+        return view('todo.index',['todoList' => $todoList]);
     }
 }
--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,7 +11,9 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
+
 <body>
+  @dd($todoList)
   <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
@@ -37,8 +40,9 @@
             </div>
           </div>
         </div>
-     </div>
+      </div>
     </main>
   </div>
 </body>
+
 </html>
