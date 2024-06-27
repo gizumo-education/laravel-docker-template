f7a88b37ded4615128b79e2d2aeed20040580b6e
ShirayukiRen <uryuucoollove@gmail.com>
2024-06-28 01:10:28 +0900

    add: 取得したデータを画面に渡す

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -11,7 +11,7 @@ class TodoController extends Controller
     {
         $todo = new Todo();
         $todoList = $todo->all();
-        dd($todoList);
-        return view('todo.index'); 
+        
+        return view('todo.index',['todoList' => $todoList]);
     }
 }
