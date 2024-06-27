07a983db6857deeafb126690be1c15b8cce60141
ShirayukiRen <uryuucoollove@gmail.com>
2024-06-28 01:03:58 +0900

    add: DBに登録されているデータを取得する

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todoList = $todo->all();
+        dd($todoList);
         return view('todo.index'); 
     }
 }
