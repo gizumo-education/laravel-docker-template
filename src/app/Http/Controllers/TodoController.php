3b9310b9b64848d34167001027aa4cfac9655d8b
Genki <gensosv@gmail.com>
2024-07-05 00:53:52 +0900

    feat: 取得したデータを画面に渡す機能を追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -11,6 +11,6 @@ class TodoController extends Controller
         $todo = new Todo();
         $todos = $todo->all();
 
-        return view('todo.index');
+        return view('todo.index', ['todos' => $todos]);
     }
 }
