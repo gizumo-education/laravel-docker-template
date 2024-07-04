0f559cda247cf1639c095f95242996c297b69b9b
sakhide <moutakusanda777@gmail.com>
2024-07-04 15:27:30 +0900

    todosテーブルからレコードを取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,6 +8,10 @@ class TodoController extends Controller
 {
     public function index()
     {
+
+        $todo = new Todo();
+        $todoList = $todo->all();
+
         return view('todo.index');
     }
 
