f1b633572113d7e68ddfb3fc92723d477fc5a4e1
Genki <gensosv@gmail.com>
2024-07-09 10:58:56 +0900

    feat: 新規作成機能の追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 
+use Illuminate\Http\Request;
+
 class TodoController extends Controller
 {
     public function index()
@@ -17,5 +19,21 @@ class TodoController extends Controller
     public function create()
     {
         return view('todo.create');
+    }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content');
+    
+
+    // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+    $todo = new Todo();
+    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+    $todo->content = $content;
+    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+    $todo->save();
+    
+    return redirect()->route('todo.index');
+
     }
 }
