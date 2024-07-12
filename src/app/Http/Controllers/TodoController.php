5bde58750a0ff53830682d0f1a7b67188e613c15
sakhide <moutakusanda777@gmail.com>
2024-07-12 18:52:32 +0900

    フォームから送信された値を一括で取得する実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -24,11 +24,12 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all();
+        dd($inputs);
 
-        
+
         $todo = new Todo();
-        $todo->content = $content;
+        $todo->fill($inputs);
         $todo->save();
 
         return redirect()->route('todo.index');
