02c47f9f2d7cdec834710655fe067a853b81e6d7
ShirayukiRen <uryuucoollove@gmail.com>
2024-07-14 22:48:14 +0900

    feat:　バリデーションの追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest; 
 use App\Todo;
-
-use function PHPUnit\Framework\returnSelf;
 
 class TodoController extends Controller
 {
@@ -13,49 +11,49 @@ class TodoController extends Controller
 
     public function __construct(Todo $todo)
     {
-        $this->todo = $todo; 
+        $this->todo = $todo;
     }
 
     public function index()
     {
         $todos = $this->todo->all();
-        
-        return view('todo.index',['todos' => $todos]);//todoList配列をtodo.indexに渡している。
+
+        return view('todo.index', ['todos' => $todos]); //todoList配列をtodo.indexに渡している。
     }
 
     public function create()
-{
-    return view('todo.create'); //.pathになっていて.でつなげていくたびにディレクトリを深く探してくれる　簡単に言うとcd todo/create の/の役割を担ってる
-}
+    {
+        return view('todo.create'); //.pathになっていて.でつなげていくたびにディレクトリを深く探してくれる　簡単に言うとcd todo/create の/の役割を担ってる
+    }
 
-public function store(Request $request) //メソッドインジェクションでインスタンス化している　クラスと依存関係がなくなる
-{
+    public function store(TodoRequest $request) //メソッドインジェクションでインスタンス化している　クラスと依存関係がなくなる
+    {
         $inputs = $request->all(); //フォームで入れられた入力値だけが入ってる　トークンと文字列
-        
-        $this->todo->fill($inputs); 
+
+        $this->todo->fill($inputs);
         $this->todo->save();
 
-        return redirect()->route('todo.index'); 
-}
-public function show($id)
-{
-    $todo = $this->todo->find($id);
+        return redirect()->route('todo.index');
+    }
+    public function show($id)
+    {
+        $todo = $this->todo->find($id);
 
-    return view('todo.show', ['todo' => $todo]);
-}
+        return view('todo.show', ['todo' => $todo]);
+    }
 
-public function edit($id)
-{
-    $todo = $this->todo->find($id);
-    return view('todo.edit',['todo'=>$todo]);
-}
+    public function edit($id)
+    {
+        $todo = $this->todo->find($id);
+        return view('todo.edit', ['todo' => $todo]);
+    }
 
-public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
-{
-    $inputs = $request->all();
-    $todo = $this->todo->find($id);
-    $todo->fill($inputs)->save();
+    public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    {
+        $inputs = $request->all();
+        $todo = $this->todo->find($id);
+        $todo->fill($inputs)->save();
 
-    return redirect()->route('todo.show', $todo->id);
-}
+        return redirect()->route('todo.show', $todo->id);
+    }
 }
