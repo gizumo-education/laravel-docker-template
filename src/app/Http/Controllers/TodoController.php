276b309bdc1fc158a0fe8c2e867b0ccfb02fc38e
MurayamaKaoru <s1robudo0918@gmail.com>
2024-07-29 00:36:56 +0900

    add:TodoControllerを追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('todo.index');
+        $todo = new Todo();
+        $todos = $todo->all();
+        dd($todos);
+
+        return view('todo.index', ['helloWorld' => 'hello World!']);
     }
 }
