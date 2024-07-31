ed2adc364e3285bea36d8c81a9fcbedeeadc1b19
ryosan1368 <horikoshi.ryo1358@gmail.com>
2024-07-31 21:13:08 +0900

    add:Eloquent ORMの設定

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+
         return view('todo.index');
     }
 }
