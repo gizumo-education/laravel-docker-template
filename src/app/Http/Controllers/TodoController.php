f5a498dd6f48cef544541ad618534e09ffc36238
ryosan1368 <horikoshi.ryo1358@gmail.com>
2024-08-15 15:15:47 +0900

    add:コントローラーのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,10 +7,16 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('todo.index', ['todos' => $todos]);
     }
@@ -24,18 +30,15 @@ class TodoController extends Controller
     {
         $inputs = $request->all();
 
-        $todo = new Todo();
-        $todo->fill($inputs);
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-
+        $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
 }
