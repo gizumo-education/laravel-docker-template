8ce914b2ed174d8a7557ab79a90fb8bdf1fef96b
v420v <ibuki420v@gmail.com>
2024-08-16 17:06:08 +0900

    feat: todo新規作成機能を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -18,5 +18,16 @@ class TodoController extends Controller
     public function create() {
         return view('todo.create');
     }
+
+    public function store(Request $request) {
+        $content = $request->input('content');
+
+        $todo = new Todo();
+        $todo->content = $content;
+
+        $todo->save();
+
+        return redirect()->route('todo.index');
+    }
 }
 
