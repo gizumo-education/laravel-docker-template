6321ec140c0f60d2d325d2183dbb2ccda66d5a9c
v420v <ibuki420v@gmail.com>
2024-08-16 16:53:26 +0900

    refactor: 共通部分をまとめたビューのベースファイルを作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
 });
 
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create');
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 
 
