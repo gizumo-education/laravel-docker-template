b02317de381c61004c70a59e4667c644c123aa1e
saito <mm.saito0912@gmail.com>
2024-08-16 18:31:46 +0900

    feat: ToDo新規作成機能 一括代入について

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -28,21 +28,41 @@ class TodoController extends Controller
         return view('todo.create'); // 追記(todoファイルの中にある、create.blade.phpを指定)
     }
 
-    public function store(Request $request) // フォームから送信されたToDoの内容を取得
+
+    // public function store(Request $request) // フォームから送信されたToDoの内容を取得
+    // {
+    //     $content = $request->input('content');
+
+    //     // dd($content);
+
+    //     // DBへの接続処理や複雑なSQLを記述することなく、DBを操作することができる
+    //     // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+    //     $todo = new Todo();
+    //     // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+    //     $todo->content = $content;
+    //     // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行(ここまでで追加されるが、ページは遷移しない)
+    //     $todo->save();
+
+    //     // リダイレクト
+    //     return redirect()->route('todo.index');
+    // }
+
+
+    // 1. フォームから送信された値を一括で取得
+    // 2. 取得した値をModelに一括で代入(※todo.phpも変更)
+    public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all(); // 変更1
+        dd($inputs); // 追記1
 
-        // dd($content);
-
-        // DBへの接続処理や複雑なSQLを記述することなく、DBを操作することができる
-        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
         $todo = new Todo();
-        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->content = $content;
-        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行(ここまでで追加されるが、ページは遷移しない)
+        $todo->fill($inputs); // 変更2
         $todo->save();
 
-        // リダイレクト
         return redirect()->route('todo.index');
     }
+
+    // (->all()を使用して、フォームから送信された値を個別ではなく一括で取得) ※変更1 追記1
+    // (連想配列で取得した値を ->fill()を使用して、Todoインスタンスの各プロパティに一括で代入) ※変更2
+    /
 }
