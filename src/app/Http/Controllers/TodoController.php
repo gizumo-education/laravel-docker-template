c227a6297be0cd9f337598e36f39828e19a0c2e9
v420v <ibuki420v@gmail.com>
2024-08-16 16:25:39 +0900

    add: Todoのモデルを追加、コントローラで全Todoをselect

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     public function index() {
+        $todo = new Todo();
+        $todos = $todo->all();
+
         return view('todo.index');
     }
 }
