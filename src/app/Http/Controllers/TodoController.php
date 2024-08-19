090dc8219c3a584fd65b84c95e687e9d6fcd48b7
v420v <ibuki420v@gmail.com>
2024-08-19 17:51:34 +0900

    feat: todo編集画面を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -37,5 +37,11 @@ class TodoController extends Controller
 
         return view('todo.show', ['todo' => $todo]);
     }
+
+    public function edit($id) {
+        $todo = $this->todo->find($id);
+
+        return view('todo.edit', ['todo' => $todo]);
+    }
 }
 
