92ac0901857b69339cd8c4a80be8df58eed0b7e4
bambashiho <bannba2356@gmail.com>
2024-09-05 17:03:42 +0900

    refactor:重複コードの共通化

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 });
 
 Route::get('/todo','TodoController@index');
-Route::get('/todo/create', 'TodoController@create');
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
