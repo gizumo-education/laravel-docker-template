de9c999f2147cdf829bd5ccc432ed02f2f804720
bambashiho <bannba2356@gmail.com>
2024-09-05 11:43:15 +0900

    feat:取得したデータを画面に渡す設定

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -14,6 +14,6 @@ class TodoController extends Controller
         $todo = new Todo();
         $todos = $todo->all();
         
-        return view('todo.index');
+        return view('todo.index', ['todos' => $todos]);
     }
 }
