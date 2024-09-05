a6bea3f6119cd8c4d535617b608c21db99f20bb0
bambashiho <bannba2356@gmail.com>
2024-09-05 12:02:39 +0900

    feat:画面上にデータを表示の追加

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -33,7 +33,11 @@
                 ToDo一覧
               </div>
               <div class="list-group list-group-flush">
-              </div>
+                @foreach($todos as $todo)
+                  <div class = "d-flex align-items-center p-s">
+                    <span class = "col-9">{{ $todo->content }}</span>
+                  </div>
+                @endforeach
             </div>
           </div>
         </div>
