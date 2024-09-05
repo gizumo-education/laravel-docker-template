7b01478f0832f77489dc406b622b11e0a0f5948c
bambashiho <bannba2356@gmail.com>
2024-09-05 11:32:10 +0900

    feat:DBに登録されているデータ取得の追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
+
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
@@ -9,6 +11,9 @@ class TodoController extends Controller
     //
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+        
         return view('todo.index');
     }
 }
