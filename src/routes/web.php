6cadaa11c04dce8f9affa3c7ddabdf4993ed5110
kameda0029 <orangemarker111@gmail.com>
2024-09-11 09:40:05 +0900

    add 詳細機能編集機能削除機能テキストファイル追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/todo', 'TodoController@index')->name('todo.index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
+Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
+Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+Route::delete('/todo/{id}', 'TodoController@delete')->name('todo.delete');
