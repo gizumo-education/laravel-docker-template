6e048b4d85f6ccd8d65fb4c0d168c8496d59bddd
KK-a-tech <kento.k711@gmail.com>
2024-09-24 18:50:27 +0900

    add:新規登録/を利用した一括代入処理の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -22,13 +22,13 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
-    
-        $todo = new Todo(); 
-        $todo->content = $content;
+        $inputs = $request->all();
+
+        $todo = new Todo();
+        $todo->fill($inputs);
         $todo->save();
 
-        return redirect()->route('todo.index'); 
+        return redirect()->route('todo.index');
     }
 
 }
