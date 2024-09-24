c44290f4d2fe04fef289b6846352f319d056e033
KK-a-tech <kento.k711@gmail.com>
2024-09-24 18:41:06 +0900

    add:追加contentの登録処理実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -19,4 +19,16 @@ class TodoController extends Controller
     {
         return view('todo.create');
     }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content');
+    
+        $todo = new Todo(); 
+        $todo->content = $content;
+        $todo->save();
+
+        return redirect()->route('todo.index'); 
+    }
+
 }
