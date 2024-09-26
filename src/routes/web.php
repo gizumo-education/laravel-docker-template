04e68f6889f987c92159755cc0489f51239342f6
KK-a-tech <kento.k711@gmail.com>
2024-09-26 15:35:00 +0900

    add:todo編集機能の実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -25,4 +25,4 @@ Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
 
-Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+Route::put('/todo/{id}', 'TodoController@update')->name('todo.update'); 
