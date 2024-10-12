810494256c1fa9836df00c5976a39257d9e9a7ef
kanata <kanata13131@gmail.com>
2024-10-12 22:56:44 +0900

    feat : バリデーションの実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\TodoRequest; // 追加
 use App\Todo; 
 use Illuminate\Http\Request;
+
 
 class TodoController extends Controller
 {
@@ -21,34 +23,34 @@ class TodoController extends Controller
         // dd('新規作成画面のルート実行成功');
         return view('todo.create',);
     }
-    public function store(Request $request)
+    public function store(TodoRequest $request)
 {
     $inputs = $request->all();
-
     $todo= $this->todo->fill($inputs);
     $todo->save();
-
     return redirect()-> route('todo.index');
 }
+
 public function show($id)
 {
     // dd($id);
     $todo = $this->todo->find($id);
     return view('todo.show', ['todo' => $todo]);
 }
+
 public function edit($id)
 {
     $todo = $this->todo->find($id);
     // dd($todo);
     return view('todo.edit', ['todo'=> $todo]);
 }
-public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+
+public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
 {
     // TODO: リクエストされた値を取得
     $inputs = $request->all();
     $todo = $this->todo->find($id);
     $todo->fill($inputs)->save();
-
     return redirect()->route('todo.show', $todo->id);
 }
 
