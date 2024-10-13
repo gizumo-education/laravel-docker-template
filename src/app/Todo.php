22b4f92617ee6ef5d215d4e783c3d8d9a5393cea
kanata <kanata13131@gmail.com>
2024-10-13 10:40:42 +0900

    reafactor : trait利用による論理削除実装

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // 追記
+
 
 class Todo extends Model
 {
+    use SoftDeletes; // 追記
     protected $table = 'todos';
 
     protected $fillable = [
