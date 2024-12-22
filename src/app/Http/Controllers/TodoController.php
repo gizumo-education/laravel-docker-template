b4bac31e44f1c3cd788a0e5e293c5c9e230efd70
miura <hgb0720@gmail.com>
2024-12-22 23:40:17 +0900

    Contrillerのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,10 +7,11 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo;
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('todo.index', ['todos' => $todos]);
     }
@@ -24,9 +25,8 @@ class TodoController extends Controller
     {
         $inputs = $request->all();
 
-        $todo = new Todo();
-        $todo->fill($inputs);
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
@@ -38,9 +38,13 @@ class TodoController extends Controller
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
+        $todo = $this->todo->find($id);
 
-        return view('todo.show', ['todo => $todo']);
+        return view('todo.show', ['todo' => $todo]);
+    }
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
     }
 }
