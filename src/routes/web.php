fad0334a118a27fe1ef763ffd762a4adffe9f6c6
miura <hgb0720@gmail.com>
2024-12-26 23:56:42 +0900

    ToDo削除機能の実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -22,3 +22,4 @@ Route::get('/test/index', 'TodoController@test')->name('test.index');
 Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
 Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+Route::delete('/todo/{id}', 'TodoController@delete')->name('todo.delete');
