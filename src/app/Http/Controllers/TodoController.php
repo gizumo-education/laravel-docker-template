003ec5c33a14b912e86a32cd10101fdb37e7961b
c.kawakami <re10naruna@gmail.com>
2025-01-29 17:34:54 +0900

    モデルインスタンスのリファクタリングを実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,10 +7,16 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    //private $todo;//TodoControllerのクラスプロパティ
+
+    //public function __construct(Todo $todo)//Todoクラスのインスタンスを生成し、$todoという変数に代入
+    //{
+        //$this->todo = $todo;
+    //}
+
     public function index()
     {
-        $todo = new Todo(); //TodoModelのインスタンスを呼び出し
-        $todos = $todo->all(); //$todoに格納されている配列を全て呼び出す
+        $todos = $this->todo->all(); //$thisでインスタンス化されたオブジェクトを全件呼び出し$todosに格納
         //第一引数で指定し、第二引数に渡したいデータを連想配列の形で渡す
         return view('todo.index', ['todos' => $todos]);
     }
@@ -24,17 +30,15 @@ class TodoController extends Controller
     { //インスタンス化が自動で行われる。メソッドインジェクション
         $inputs = $request->all();//フォームから送信された値を個別ではなく一括で取得
 
-        $todo = new Todo();//TodoModelのインスタンスを呼び出し
-        $todo->fill($inputs);//指定した連想配列を一括代入
-        $todo->save();//save()を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $this->todo->fill($inputs);//指定した連想配列を一括代入
+        $this->todo->save();//save()を実行してオブジェクトの状態をDBに保存するINSERT文を実行
 
         return redirect()->route('todo.index');//処理が終われば、index.phpのページに遷移される。
     }
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
+        $todo = $this->todo->find($id);
         
         return view('todo.show', ['todo' => $todo]);
     }
