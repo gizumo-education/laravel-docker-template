7a8dde00f5d819174129610c156720f2c85423fc
c.kawakami <re10naruna@gmail.com>
2025-01-30 20:51:33 +0900

    データ更新を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -50,4 +50,15 @@ class TodoController extends Controller
         
         return view('todo.edit', ['todo' => $todo]);
     }
+
+    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得 第2引数: ルートパラメータの取得
+    {
+        $inputs = $request->all();
+        // TODO: 更新対象のデータを取得
+        $todo = $this->todo->find($id);
+        // TODO: 更新したい値の代入とUPDATE文の実行
+        $todo->fill($inputs)->save();
+
+        return redirect()->route('todo.show', $todo->id);
+    }
 }
