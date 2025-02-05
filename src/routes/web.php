5194a49ef3963d1ee471f4067e835e8d5863f018
c.kawakami <re10naruna@gmail.com>
2025-02-05 12:18:44 +0900

    削除ボタンの実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -22,5 +22,5 @@ Route::post('/todo', 'TodoController@store')->name('todo.store');
 Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');//ルートパラメータ｛｝内の変数
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
 Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
-Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
 Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
+Route::delete('/todo/{id}', 'TodoController@delete')->name('todo.delete');
