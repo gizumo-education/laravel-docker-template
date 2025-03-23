8169e3c1052d2b54fb374898444a0b2749312602
yuta20253 <magee.20253@gmail.com>
2025-03-23 17:49:17 +0900

    add: Todo新規作成ページを作成・表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -13,4 +13,9 @@ class TodoController extends Controller
         $todos = $todo->all();
         return view('todo.index', ['todos' => $todos]);
     }
+
+    public function create()
+    {
+        return view('todo.create');
+    }
 }
