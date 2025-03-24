f4f96df0d2e09aa66fca16e23332d24ffb4627a8
TakiumiMurakami <takumiaf4@gmail.com>
2025-03-24 11:02:55 +0900

    add: Todo.phpの追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+
+
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
         return view('todo.index');
     }
-    //
+    //class Todo extends Modelのインスタンス化
 }
---
+++ src/app/Todo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    //セクション８追加
+    protected $table = 'todos';
+}
