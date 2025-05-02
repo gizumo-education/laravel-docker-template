6c1eb365c2ee6cb07b389a9deefddaac065d9574
TakiumiMurakami <takumiaf4@gmail.com>
2025-05-02 13:05:17 +0900

    refactor:TodoController.phpのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -10,10 +10,16 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo; 
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index() //初期画面
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
         return view('todo.index', ['todos' => $todos]); 
     }
     //class Todo extends Modelのインスタンス化
@@ -24,22 +30,30 @@ class TodoController extends Controller
         return view('todo.create' );
     }
 
-    public function store(Request $request) //クラスを引数に指定
+    public function store(Request $request) 
     {
-        $inputs = $request->all(); 
-        $todo = new Todo(); 
-        $todo->fill($inputs);
-        $todo->save();
+        $inputs = $request->all();
+        $this->todo->fill($inputs);
+        $this->todo->save();
         return redirect()->route('todo.index');
-    }
+    }//storeメソッドの中身を調べる
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
+        $todo = $this->todo->find($id);
        //dd($todo);
        return view('todo.show', ['todo' => $todo]); 
     }
-}//storeメソッドの中身を調べる
+}
 
 
+
+// class TodoController extends Controller
+// {
+//     private $todo; // 追記
+
+//     public function __construct(Todo $todo)
+//     {
+//         $this->todo = $todo; // 追記
+//     }
+// }見直す
--- src/app/Todo.php
+++ src/app/Todo.php
@@ -12,4 +12,6 @@ class Todo extends Model
     protected $fillable = [
         'content',
     ];
+    //こうすることで、画面の入力項目が増減しても常に同じコードで登録処理が実現できるようになり保守性と可読性が向上しました。
+
 }
