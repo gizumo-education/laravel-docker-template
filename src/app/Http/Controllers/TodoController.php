fc2a5a8432339e9b994ab648b3e783b74bd58650
TakiumiMurakami <takumiaf4@gmail.com>
2025-05-08 00:11:01 +0900

    feat:text欄のバリデーションの実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests\TodoRequest; // 追加
 
 use App\Todo;
 
@@ -30,7 +31,7 @@ class TodoController extends Controller
         return view('todo.create' );
     }
 
-    public function store(Request $request) 
+    public function store(TodoRequest $request) 
     {
         $inputs = $request->all();
         $this->todo->fill($inputs);
@@ -53,7 +54,7 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]); 
     }
 
-    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
     {
         // TODO: リクエストされた値を取得
         $inputs = $request->all();
