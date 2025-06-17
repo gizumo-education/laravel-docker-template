f149274fb60d650a23a0afb4351f9bf3fecb6dd8
arisa2919 <nwtr310@gmail.com.com>
2025-06-17 22:50:11 +0900

    rooting /todo to TodoController@index

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/todo', function () {
+    echo 'Hello World!';
+});
+
+Route::get('/todo', 'TodoController@index');
