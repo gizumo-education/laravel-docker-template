c914924a89c8bb4971c2f64002eb5404c4d862c7
arisa2919 <nwtr310@gmail.com.com>
2025-06-18 17:13:10 +0900

    余分な行の削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,5 +21,4 @@ Route::get('/todo', function () {
 });
 
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create'); 
-Route::get('/todo/create', 'TodoController@create')->name('todo.create');
+Route::get('/todo/create', 'TodoController@create');->name('todo.create');
