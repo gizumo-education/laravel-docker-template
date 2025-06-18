a68d0f7de4d1bef602e0a6e0368d912ebf74f154
arisa2919 <nwtr310@gmail.com.com>
2025-06-18 16:50:04 +0900

    add: 新規作成画面のルート実行!の表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -15,4 +15,10 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]); 
     }
+    public function create()
+    {
+        dd('新規作成画面のルート実行！');
+    }
+    
 }
+
