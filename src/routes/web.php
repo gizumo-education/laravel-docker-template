d020c024015820a631faea0eff6f4981709422b0
arisa2919 <nwtr310@gmail.com.com>
2025-06-18 21:09:39 +0900

    feat: データ保存時のルート定義

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,6 +19,9 @@ Route::get('/', function () {
 Route::get('/todo', function () {
     echo 'Hello World!';
 });
-
+// 一覧表示
 Route::get('/todo', 'TodoController@index');
+// 追加ボタン押した時（新規作成時）
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
+// データ保存時
+Route::post('/todo', 'TodoController@store')->name('todo.store');
