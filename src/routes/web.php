3c758aef54fd333f28cf32de674e618d172d337c
arisa2919 <nwtr310@gmail.com.com>
2025-06-18 19:00:19 +0900

    add: base.blade.php（共通部分）追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,4 +21,4 @@ Route::get('/todo', function () {
 });
 
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create');->name('todo.create');
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
