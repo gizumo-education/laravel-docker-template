ac7a4e7def50cdf03e017eb3f024166940f78e5f
arisa2919 <nwtr310@gmail.com.com>
2025-06-20 22:33:51 +0900

    feat: 詳細画面表示のルート定義

--- src/routes/web.php
+++ src/routes/web.php
@@ -25,3 +25,5 @@ Route::get('/todo', 'TodoController@index')->name('todo.index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 // データ保存
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+// 詳細画面の表示
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
