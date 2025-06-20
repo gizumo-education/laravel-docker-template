ae834ff7978767e4493d6501a29596938221a0ba
arisa2919 <nwtr310@gmail.com.com>
2025-06-20 22:37:15 +0900

    add:詳細ボタンのURLの追加

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -16,7 +16,7 @@
                   <div class="d-flex align-items-center p-2">
                     <span class="col-9">{{ $todo->content }}</span>
                   </div>
-                  <a href="" class="btn btn-info ml-3">詳細</a>
+                  <a href="{{ route('todo.show', $todo->id) }}" class="btn btn-info ml-3">詳細</a>
                 @endforeach
               </div>
             </div>
