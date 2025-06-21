51d79bce4411db50168019295b805e94b0656730
arisa2919 <nwtr310@gmail.com.com>
2025-06-21 23:49:10 +0900

    fix: バリデーションを利用の為、TodoRequestクラスを継承

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -58,7 +58,7 @@ class TodoController extends Controller
         // TODO: view()を使用して編集画面を表示
         return view('todo.edit', ['todo' => $todo]); 
     }
-    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
     {
     // TODO: リクエストされた値を取得
     $inputs = $request->all();
