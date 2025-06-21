5f6c3bd70ab13b21bb4da4d9174dbdd0a99b21f4
arisa2919 <nwtr310@gmail.com.com>
2025-06-21 23:45:15 +0900

    add:バリデーションのためTodoRequest.phpの追加

---
+++ src/app/Http/Requests/TodoRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TodoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // リクエストを受け付けるためtrueに変更
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            // 入力可能な文字数の設定（バリデーション）
+            'content' => 'required|max:255',
+        ];
+    }
+}
