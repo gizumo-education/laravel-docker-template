85b73677b3237c7e4fcc13c2cab2c23d776699be
arisa2919 <nwtr310@gmail.com.com>
2025-06-21 10:51:49 +0900

    feat: 編集画面表示のためeditメソッドの追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -50,7 +50,14 @@ class TodoController extends Controller
     // 詳細画面と指定IDのコンテンツを表示
     return view('todo.show', ['todo' => $todo]); 
     }
-    
+    // TODO: ルートパラメータを引数に受け取る
+    public function edit($id)
+    {
+        // TODO: 編集対象のレコードの情報を持つTodoモデルのインスタンスを取得
+        $todo = $this->todo->find($id);
+        // TODO: view()を使用して編集画面を表示
+        return view('todo.edit', ['todo' => $todo]); 
+    }
    
 }
 
