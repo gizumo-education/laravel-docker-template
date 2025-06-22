129bf2a9a7108e6a9e3c6bf8814fedb4334c5d20
arisa2919 <nwtr310@gmail.com.com>
2025-06-22 22:25:06 +0900

    feat: データ復元のためTodoに論理削除の設定

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; 
 
 class Todo extends Model
 {
+    use SoftDeletes;
     // テーブル操作のためのマッピング
     protected $table = 'todos';
     // 一括代入を行うカラムの指定
