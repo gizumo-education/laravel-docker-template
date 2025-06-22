246e8cddbf4ecc644b4337ddea87f63f6d94b121
arisa2919 <nwtr310@gmail.com.com>
2025-06-22 21:15:15 +0900

    add: 削除ボタンのルート定義

--- src/resources/views/todo/show.blade.php
+++ src/resources/views/todo/show.blade.php
@@ -15,8 +15,9 @@
           <a href="{{ route('todo.edit', $todo->id) }}" class="btn btn-info">編集する</a>
         </div>
         <div class="col-auto">
-    <form method="POST" action="">
-      @csrf
+    <form method="POST" action="{{ route('todo.delete', $todo->id) }}">
+        @method('DELETE')
+        @csrf
       <button type="submit" class="btn btn-danger">削除する</button>
     </form>
   </div>
