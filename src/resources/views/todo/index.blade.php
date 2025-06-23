b855e32402a515e45ff7eedcbf31c74b686b4dfd
ishikawakou <glecter4.3@gmail.com>
2025-06-23 17:33:00 +0900

    - feat 取得したデータを画面に渡す

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -12,33 +12,33 @@
 	</head>
 	<body>
 		<div id="app">
-		<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-		<div class="container">
-			<a class="navbar-brand" href="/todo">ToDo</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-			<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav mr-auto"></ul>
-			<ul class="navbar-nav ml-auto"></ul>
-			</div>
-		</div>
-		</nav>
-		<main class="py-4">
-		<div class="container">
-			<div class="row justify-content-center">
-			<div class="col-md-8">
-				<div class="card">
-				<div class="card-header">
-					ToDo一覧
+			<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+				<div class="container">
+					<a class="navbar-brand" href="/todo">ToDo</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto"></ul>
+						<ul class="navbar-nav ml-auto"></ul>
+					</div>
 				</div>
-				<div class="list-group list-group-flush">
+			</nav>
+			<main class="py-4">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-md-8">
+							<div class="card">
+								<div class="card-header">
+									ToDo一覧
+								</div>
+								<div class="list-group list-group-flush">
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				</div>
-			</div>
-			</div>
-		</div>
-		</main>
+			</main>
 		</div>
 	</body>
 </html>
