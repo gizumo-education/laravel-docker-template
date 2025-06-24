c17010571277452cb0a619bce655a8aa15092697
kirari <kirakira24xox12@gmail.com>
2025-06-24 10:44:21 +0900

    allに変更で一括取得とfillで一括代入

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -23,12 +23,12 @@ class TodoController extends Controller
 
     public function store(Request $request)
 {
-    $content = $request->input('content');
+    $inputs = $request->all();
 
     // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-    $todo = new Todo(); 
+    $todo = new Todo();
     // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-    $todo->content = $content;
+    $todo->fill($inputs);
     // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
     $todo->save();
 
