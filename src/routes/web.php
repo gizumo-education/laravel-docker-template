a44cf96711553e0efcd8e67c934c8321219ace4e
kirari <kirakira24xox12@gmail.com>
2025-07-04 14:15:00 +0900

    詳細画面表示機能のルートの設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,3 +19,5 @@ Route::get('/todo','TOdoController@index')->name('todo.index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
