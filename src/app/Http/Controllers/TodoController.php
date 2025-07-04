c9412dd4a8a39313905451275ce8062baf252627
kirari <kirakira24xox12@gmail.com>
2025-07-04 14:49:38 +0900

    Controllerのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,10 +8,16 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('todo.index',['todos' => $todos]);
     }
@@ -26,20 +32,23 @@ class TodoController extends Controller
         $inputs = $request->all();
 
     // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-        $todo = new Todo();
+        // $todo = new Todo();
     // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->fill($inputs);
+        // $todo->fill($inputs);
     // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-        $todo->save();
+        // $todo->save();
+
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-
+        // $model = new Todo();
+        // $todo = $model->find($id);
+        $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
     
