77cb02cc79c536d546e7888cdb5b23d1b35f4eb2
tomomi ueno <chimdon.0410@gmail.com>
2025-07-08 19:55:58 +0900

    feat:web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/todo', function () {
+    echo 'Hello World!';
+}); 
+
 
 Route::get('/', function () {
     return view('welcome');
