a945ff59a8dfae19193006ac8c912a8ed3e90149
kirari <kirakira24xox12@gmail.com>
2025-07-09 12:35:25 +0900

    更新機能のルートの設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,9 +18,12 @@ Route::get('/', function () {
 Route::get('/todo','TOdoController@index')->name('todo.index');
 
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
-
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+
 
 Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
+
+Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
