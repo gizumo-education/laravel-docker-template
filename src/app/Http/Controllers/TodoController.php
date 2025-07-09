8286d23319d26b0fef16c6f5cd12eeaca9f8f701
kirari <kirakira24xox12@gmail.com>
2025-07-09 11:11:42 +0900

    編集対象のデータの取得と編集画面へ表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -52,5 +52,10 @@ class TodoController extends Controller
         return view('todo.show', ['todo' => $todo]);
     }
     
+    public function edit($id)
+    {
+        $todo =$this->todo->find($id);
+        return view('todo.edit',['todo' =>$todo]);
+    }
 }
 
