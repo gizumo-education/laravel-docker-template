4c2987469ecd6fb5b9995fb1e18b4fa3b7f29c2b
ishikawakou <glecter4.3@gmail.com>
2025-07-15 15:29:47 +0900

    feat DBにデータを保存する

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', function () {
 
 Route::get('/todo', 'TodoController@index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
+Route::post('/todo', 'TodoController@store')->name('todo.store');
+Route::get('/todo', 'TodoController@index')->name('todo.index');
