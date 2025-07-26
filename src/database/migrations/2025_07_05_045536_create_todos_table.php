e61e030436fe31e9af9f1536c1729f3f5ed56db7
tomomi ueno <chimdon.0410@gmail.com>
2025-07-26 09:12:34 +0900

    add: construct

--- src/database/migrations/2025_07_05_045536_create_todos_table.php
+++ src/database/migrations/2025_07_05_045536_create_todos_table.php
@@ -18,6 +18,7 @@ class CreateTodosTable extends Migration
             $table->string('content');
             $table->timestamps();
         });
+        
     }
 
     /**
--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TodoSeeder::class,
         ]);
+       
     }
 }
