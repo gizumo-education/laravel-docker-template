16f16b66659c9158bc9791f101dd4c4f8ca4360a
yi4695 <arikanasai.real1a@gmail.com>
2025-07-27 14:07:23 +0900

    add: Seederファイルの作成

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            TodoSeeder::class,
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
