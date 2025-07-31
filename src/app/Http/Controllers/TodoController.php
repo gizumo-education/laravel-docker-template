ed4f18307a8e3271e6dd9dd55eed122652c0828d
yi4695 <arikanasai.real1a@gmail.com>
2025-07-31 19:37:55 +0900

    feat: ToDo新規作成機追加　新規登録,一覧へﾘﾀﾞｲﾚｸﾄ,CSRF対策

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -21,7 +21,23 @@ class TodoController extends Controller
 
     public function create()
     {
-    // dd('新規作成画面のルート実行！');
-    return view('todo.create');
+        // dd('新規作成画面のルート実行！');
+        return view('todo.create');
+    }
+
+    public function store(Request $request)
+    {
+        // dd('新規作成のルート実行！');
+        $content = $request->input('content');
+        //dd($content);
+        
+        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+        $todo = new Todo();
+        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->content = $content;
+        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->save();
+
+        return redirect()->route('todo.index');
     }
 }
