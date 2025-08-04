b147a0351f840834fc0dc4a86cbdc05df608c013
ishikawakou <glecter4.3@gmail.com>
2025-08-04 13:48:20 +0900

    feat: 編集画面の表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -38,4 +38,9 @@ class TodoController extends Controller
 		$todo = $this->todo->find($id);
 		return view('todo.show', ['todo' => $todo]);
 	}
+	public function edit($id)
+	{
+		$todo = $this->todo->find($id);
+		return view('todo.edit', ['todo' => $todo]);
+	}
 }
