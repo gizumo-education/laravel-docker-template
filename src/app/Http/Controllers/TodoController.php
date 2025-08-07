ac71733a8f408c5be14c81b4bd266ce3d5a821ea
kakaomame1100 <kayanod.gthnskiym0329@gmail.com>
2025-08-07 15:40:26 +0900

    add:詳細画面の表示実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -10,16 +10,21 @@ class TodoController extends Controller
     public function index()
     {
         $todo = new Todo();
+        // Todoクラスとは？
         $todos = $todo->all();
         return view('todo.index', ['todos' => $todos]);
+        // view関数の文法について
     }
     public function create()
     {
         return view('todo.create');
     }
     public function store(Request $request)
+    //引数の左隣にクラスを入れて自動インスタンス化
     {
     $inputs = $request->all();
+    /* シングルアローはインスタンスの中のプロパティなどを呼ぶときに使用（$requestはオブジェクト型）
+    $requestに何クラスからできたインスタンス化か*/
     // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
     $todo = new Todo();
     // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
@@ -27,5 +32,11 @@ class TodoController extends Controller
     // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
     $todo->save();
     return redirect()->route('todo.index');
+    }
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+        return view('todo.show', ['todo' => $todo]);
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/todo', function () {
     echo 'Hello World!';
 });
 
-Route::get('/todo', 'TodoController@index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 Route::get('/todo', 'TodoController@index')->name('todo.index');
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
