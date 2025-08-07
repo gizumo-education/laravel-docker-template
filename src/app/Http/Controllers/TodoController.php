93ee4cc4442fed4a93b5a431bd4613f82d7f7ddd
kakaomame1100 <kayanod.gthnskiym0329@gmail.com>
2025-08-07 15:56:22 +0900

    refactor:Controllerのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,13 +7,15 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    private $todo;
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
     public function index()
     {
-        $todo = new Todo();
-        // Todoクラスとは？
-        $todos = $todo->all();
-        return view('todo.index', ['todos' => $todos]);
-        // view関数の文法について
+    $todos = $this->todo->all();
+    return view('todo.index', ['todos' => $todos]);
     }
     public function create()
     {
@@ -25,18 +27,13 @@ class TodoController extends Controller
     $inputs = $request->all();
     /* シングルアローはインスタンスの中のプロパティなどを呼ぶときに使用（$requestはオブジェクト型）
     $requestに何クラスからできたインスタンス化か*/
-    // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-    $todo = new Todo();
-    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-    $todo->fill($inputs);
-    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-    $todo->save();
+    $this->todo->fill($inputs);
+    $this->todo->save();
     return redirect()->route('todo.index');
     }
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-        return view('todo.show', ['todo' => $todo]);
+    $todo = $this->todo->find($id);
+    return view('todo.show', ['todo' => $todo]);
     }
 }
