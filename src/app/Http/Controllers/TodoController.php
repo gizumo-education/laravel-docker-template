c9a093cf5e1663db5e0958949e9abb08f73876c8
ishikawakou <glecter4.3@gmail.com>
2025-08-08 14:09:39 +0900

    feat; DBのデータを更新する

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -43,4 +43,11 @@ class TodoController extends Controller
 		$todo = $this->todo->find($id);
 		return view('todo.edit', ['todo' => $todo]);
 	}
+	public function update(Request $request, $id)
+	{
+		$inputs = $request->all();
+		$todo = Todo::find($id);
+		$todo->fill($inputs)->save();
+		return redirect()->route('todo.show', $todo->id);
+	}
 }
