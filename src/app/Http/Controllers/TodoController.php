158604b5bdd79eb9fc4ef54b40c5b8ef1c6050db
tamayose-daisuke <tmysdisk@gmail.com>
2025-10-11 15:01:05 +0900

    feat:ToTo新規作成ページの作成・表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -14,4 +14,10 @@ class TodoController extends Controller
 
       return view('todo.index', ['todos' =>$todos]);
     }
+    public function create()
+    {
+    // TODO: 第1引数を指定
+    return view('todo.create'); 
+    }
 }
+
