b44ab38d05eb0765f51dd5d9a4332236e7169c05
tamayose-daisuke <tmysdisk@gmail.com>
2025-10-29 20:13:32 +0900

    feat: コンストラクタインジェクションを使い、各メソッドのインスタンス化ノリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,11 +7,17 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    private $todo; //クラスプロパティ 
+
+    public function __construct(Todo $todo) //コンストラクタインジェクション
+    {
+      $this->todo = $todo;
+    }
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
-        return view('todo.index', ['todos' => $todos]);
+      $todos = $this->todo->all();
+
+      return view('todo.index', ['todos' => $todos]);
     }
     public function create()
     {
@@ -20,16 +26,13 @@ class TodoController extends Controller
     public function store(Request $request)
     {
       $inputs = $request->all();
-      $todo = new Todo();
-      $todo->fill($inputs); // 変更
-      $todo->save();
+      $this->todo->fill($inputs); // 変更
+      $this->todo->save(); 
       return redirect()->route('todo.index');
     }
     public function show($id)
     {
-      $model = new Todo();
-      $todo = $model->find($id);
-
-      return view('todo.show', ['todo' => $todo]);
+    $todo = $this->todo->find($id);
+    return view('todo.show', ['todo' => $todo]);
     }
 }
