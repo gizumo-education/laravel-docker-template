0a02efac6e7e24d3c2bda7c0062baa6169da05b9
shimami <oshima.m0805@gmail.com>
2025-11-20 16:57:09 +0900

    ステージング範囲の修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todo', function () {
-    echo 'Hello World!';
-});
+Route::get('/todo', 'TodoController@index');
 
 ?>
