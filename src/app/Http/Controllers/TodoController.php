d30800c41cb1922dc0cde63b4506d68e7b85ba31
Mchihiro0529 <glass7kurage@gmail.com>
2025-11-24 19:21:34 +0900

    add:一覧表示機能の作成

---
+++ src/app/Http/Controllers/TodoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        return view('todo.index');
+    }
+
+}
