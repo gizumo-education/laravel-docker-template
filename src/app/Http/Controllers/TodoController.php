bbf41a3cdfc3b0119b9c5172fff5a5b5432cefff
Mchihiro0529 <glass7kurage@gmail.com>
2025-11-25 22:52:18 +0900

    add:DBに登録されているデータの取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
+
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+        dd($todos);
+
         return view('todo.index');
     }
 
