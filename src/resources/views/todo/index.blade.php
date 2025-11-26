8dbf0edb6c72e25fff64b384da7077a84b89d054
Mchihiro0529 <glass7kurage@gmail.com>
2025-11-26 20:17:07 +0900

    add:画面上にデータを表示する

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -33,7 +33,12 @@
                 ToDo一覧
               </div>
               <div class="list-group list-group-flush">
-              </div>
+                @foreach ($todos as $todo)
+                  <div class="d-flex align-items-center p-2">
+                    <span class="col-9">{{ $todo->content }}</span>
+                  </div>
+                @endforeach
+              </div> 
             </div>
           </div>
         </div>
