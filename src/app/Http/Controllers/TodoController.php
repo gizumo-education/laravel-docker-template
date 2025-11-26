eb32d9dbe61018251fd3a438fc2ef9380384a8f0
Mchihiro0529 <glass7kurage@gmail.com>
2025-11-26 20:01:13 +0900

    add:取得したデータを画面に渡す

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -12,9 +12,9 @@ class TodoController extends Controller
     {
         $todo = new Todo();
         $todos = $todo->all();
-        dd($todos);
+        // dd($todos);
 
-        return view('todo.index');
+        return view('todo.index', ['todos' => $todos]);
     }
 
 }
