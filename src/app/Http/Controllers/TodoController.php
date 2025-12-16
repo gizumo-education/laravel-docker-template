1b800644fcfac1fbc6be51c57be18f71a3abb0cd
kimuraAyuri <ayurin533@icloud.com>
2025-12-16 20:22:06 +0900

    add: DBのデータ取得処理

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+        dd($todos);
+        
         return view('todo.index');
     }
 }
