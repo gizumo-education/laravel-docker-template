6767397b9750b816d16f188b21bd954d03b99c11
kimuraAyuri <ayurin533@icloud.com>
2025-12-16 20:36:28 +0900

    add: 取得したデータを画面に渡す処理の追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -12,8 +12,7 @@ class TodoController extends Controller
     {
         $todo = new Todo();
         $todos = $todo->all();
-        dd($todos);
-        
-        return view('todo.index');
+
+        return view('todo.index', ['todos' => $todos]);
     }
 }
