0751d8d713ef96ee32ff088aa237e5019dd28921
hiroki20001002 <hirokikouno111@gmail.com>
2026-01-06 16:42:45 +0900

    feat: データ取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all(); //todosテーブルから全てのレコードを取得
+
         return view('todo.index');
     }
 }
