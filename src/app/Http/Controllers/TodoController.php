f3c2444017f8f33757b0292d7ebe0bc6aa3022b9
hiroki20001002 <hirokikouno111@gmail.com>
2026-01-09 14:15:07 +0900

    add :新規作成画面の表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -15,4 +15,11 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]);
     }
+
+    public function create()
+    {
+        // dd('新規作成画面のルート実行！');
+
+        return view('todo.create');
+    }
 }
