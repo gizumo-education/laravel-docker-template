11a35029e713f27d668787c5d172acb59dcd65e7
yi4695 <arikanasai.real1a@gmail.com>
2026-01-15 20:13:20 +0900

    feat: ToDoの削除処理 - 一覧画面へリダイレクト

--- src/routes/web.php
+++ src/routes/web.php
@@ -29,3 +29,4 @@ Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
 
 Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
 
+Route::delete('/todo/{id}', 'TodoController@delete')->name('todo.delete');
