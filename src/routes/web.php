87f9d7749cb12a0795bea78d911ac517ffe81883
mayu08980 <rgb08980@gmail.com>
2026-02-04 23:50:45 +0900

    feat: Todo一覧画面を表示

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/todo', function () {
-    echo 'Hello World!';
-});
+// Route::get('/todo', function () {
+//     echo 'Hello World!';
+// });
+
+Route::get('/todo', 'TodoController@index');
