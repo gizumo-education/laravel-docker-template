7886bb4472295d3590c1c3c0ef2a2c2cdfe4132c
mayu08980 <rgb08980@gmail.com>
2026-02-05 20:09:02 +0900

    feat: DBに登録されているTodoデータを取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     //
         public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+        // dd($todos);
         return view('todo.index');
     }
 }
+
