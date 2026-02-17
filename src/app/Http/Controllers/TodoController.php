a985ced02d06d5f262d594d673efdf306e7cbc3b
Haruka Shimura <y29814021@gmail.com>
2026-02-17 20:03:57 +0900

    DBに登録されているデータを取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     public function index()
     {
+       
+        $todo = new Todo();
+        $todos = $todo->all();
+       
         return view('todo.index'); 
     }
 }
