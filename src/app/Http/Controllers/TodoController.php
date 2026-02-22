849763dcbf2f5c25639f816fc54aacc03e391338
Feng <fukiarita@FUKI-MacBook-Pro.local>
2026-02-22 18:08:15 +0900

    feat: ToDo新規作成処理（保存・リダイレクト）を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Todo;
 
 class TodoController extends Controller
@@ -13,4 +14,21 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]);
     }
+    public function create()
+    {
+        return view('todo.create');
+    }
+    public function store(Request $request)
+{
+    $content = $request->input('content');
+
+    // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+    $todo = new Todo(); 
+    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+    $todo->content = $content;
+    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+    $todo->save();
+
+    return redirect()->route('todo.index');
+}
 }
