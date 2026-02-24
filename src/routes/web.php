6fabeddf32711df0be09d1f651b07f985a8aaa88
st11040319 <st.abc0319@gmail.com>
2026-02-25 08:56:07 +0900

    名前付きルート生成とBladeの分割と継承

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 
 // 編集
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create'); // 追記
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');// 追記
