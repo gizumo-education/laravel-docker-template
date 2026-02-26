b942dc5d6156d3259901d5d5b73db8d99b17a4cc
st11040319 <st.abc0319@gmail.com>
2026-02-26 20:47:20 +0900

    一括代入

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -29,16 +29,14 @@ class TodoController extends Controller
 
     public function store(Request $request)
  {
-    $content = $request->input('content'); // 追記
+   $inputs = $request->all(); // 変更
 
-    // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-    $todo = new Todo(); 
-    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-    $todo->content = $content;
-    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+    $todo = new Todo();
+    $todo->fill($inputs); // 変更
     $todo->save();
 
-    return redirect()->route('todo.index'); // 追記
+    return redirect()->route('todo.index');   
+
  }
 
 
--- src/app/Todo.php
+++ src/app/Todo.php
@@ -8,4 +8,8 @@ class Todo extends Model
 {
    // 追加
     protected $table = 'todos';
+    // 追記
+    protected $fillable = [
+        'content',
+    ];
 }
