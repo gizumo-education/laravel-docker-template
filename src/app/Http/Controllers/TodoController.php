d77a10e4b7c169efe8d7bcfa35b5792ac783ba67
tomtimtom000407-star <tomtimtom000407@gmail.com>
2026-03-01 19:31:57 +0900

    取得したデータを画面に渡す設定

---
+++ src/app/Http/Controllers/TodoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+     public function index()
+    {
+    
+        $todo = new Todo();
+          $todos = $todo->all();
+
+        return view('todo.index', ['todos' => $todos]);
+    }
+}
