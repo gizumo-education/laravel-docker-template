aebddd430240fdf238add5aee2a664453fe0512b
Haruka Shimura <y29814021@gmail.com>
2026-03-03 00:01:41 +0900

    取得した値を一括代入

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -24,12 +24,14 @@ class TodoController extends Controller
 
 public function store(Request $request)
 {
-    $content = $request->input('content');
+    //フォームから送信された値を一括で取得
+    $inputs = $request->all();
+    dd($inputs);
 
      // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
     $todo = new Todo(); 
     // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-    $todo->content = $content;
+    $todo->content = $inputs['content'];
     // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
     $todo->save();
 
