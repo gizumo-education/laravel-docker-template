7cbbb9fd97c753c80d9c6c80c4529affd074a48f
tomtimtom000407-star <tomtimtom000407@gmail.com>
2026-03-11 19:08:31 +0900

    名前付きルート定義

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -12,6 +11,7 @@ class TodoController extends Controller
     
         $todo = new Todo();
           $todos = $todo->all();
+           $todos;
 
         return view('todo.index', ['todos' => $todos]);
     }
@@ -20,6 +20,15 @@ class TodoController extends Controller
 public function create()
 {
    return view('todo.create'); 
+}
+
+public function store(Request $request)
+{
+   $todo = new Todo();
+    $todo->content = $content;
+    $todo->save();
+
+     return redirect()->route('todo.index');
 }
 }
 
