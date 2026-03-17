05edbd7c81ec4adc1301e4a3607d29c405912064
st11040319 <st.abc0319@gmail.com>
2026-03-17 09:12:18 +0900

    バリデーションを実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-// 追加
+use App\Http\Requests\TodoRequest; // 追加
 use App\Todo;
+
+// use Illuminate\Http\Request;
+
+// // 追加
+// use App\Todo;
 
 
 
@@ -34,7 +37,7 @@ class TodoController extends Controller
       return view('todo.create'); // 追記
    }
 
-   public function store(Request $request)
+   public function store(TodoRequest $request)
    {
       $inputs = $request->all(); // 変更
 
@@ -53,8 +56,19 @@ class TodoController extends Controller
    public function edit($id)
 {
     // TODO: 編集対象のレコードの情報を持つTodoモデルのインスタンスを取得
-    $todo = todo::find($id);
+    $todo = $this->todo->find($id);
     return view('todo.edit', ['todo' => $todo]);
 }
+
+   public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+   {
+      // TODO: リクエストされた値を取得
+      $inputs = $request->all();
+      $todo = $this->todo->find($id);
+      $todo->fill($inputs)->save();
+
+      return redirect()->route('todo.show', $todo->id); // 追記
+
+   }
 
 }
--- src/resources/views/todo/edit.blade.php
+++ src/resources/views/todo/edit.blade.php
@@ -5,12 +5,16 @@
     <div class="card">
       <div class="card-header">ToDo編集</div>
       <div class="card-body">
-        <form method="POST" action="">
+        <form method="POST" action="{{ route('todo.update', $todo->id) }}">
           @csrf
+          @method('PUT') <!-- ここを追加 -->
           <div class="form-group row">
             <label for="name" class="col-md-4 col-form-label text-md-right">ToDo入力</label>
             <div class="col-md-6">
-              <input type="text" class="form-control" name="content" value="{{ $todo->content }}">
+              <input type="text" class="form-control @if($errors->has('content')) border-danger @endif" name="content" value="{{ $todo->content }}">
+              @if($errors->has('content'))
+               <span class="text-danger">{{ $errors->first('content') }}</span>
+              @endif
             </div>
           </div>
           <div class="form-group row mb-0">
