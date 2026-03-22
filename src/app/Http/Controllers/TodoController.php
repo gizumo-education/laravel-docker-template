1ac67f2fbab6fc1c0c48275f0b21c95ea32f2ef0
Feng <fukiarita@FUKI-MacBook-Pro.local>
2026-03-23 00:14:43 +0900

    feat: ToDo詳細画面の実装（ルートパラメータ・Controller・Blade追加）

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -20,12 +20,19 @@ class TodoController extends Controller
         return view('todo.create');
     }
     public function store(Request $request)
-{
+    {
     $inputs = $request->all();
 
     $todo = new Todo();
     $todo->fill($inputs);
     $todo->save();
     return redirect()->route('todo.index');
-}
+    }
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+
+        return view('todo.show', ['todo' => $todo]);
+    }
 }
