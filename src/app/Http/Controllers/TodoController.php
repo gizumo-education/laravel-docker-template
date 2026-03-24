141045d2274e3cd93f9e8e2eada83112fde1a075
Haruka Shimura <y29814021@gmail.com>
2026-03-24 20:54:43 +0900

    Todoクラスインスタンス化の部分のControllerリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -9,12 +9,18 @@ use App\Todo;
 class TodoController extends Controller
 {
 
+    private $todo;
+    
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     //データ取得し一覧画面の表示するために値を渡す
     public function index()
     {
        
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
        
         return view('todo.index', ['todos' => $todos]); 
     }
@@ -30,12 +36,8 @@ public function store(Request $request)
     //フォームから送信された値を一括で取得
     $inputs = $request->all();
 
-     // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-    $todo = new Todo(); 
-    // 2. Todoインスタンスの各プロパティに保存したい値を一括で代入
-    $todo->fill($inputs);
-    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-    $todo->save();
+    $this->todo->fill($inputs);
+    $this->todo->save();
 
     return redirect()->route('todo.index');
 }
@@ -43,9 +45,7 @@ public function store(Request $request)
 //詳細取得
 public function show($id)
 {
-    $model = new Todo();
-    $todo = $model->find($id);
-
+    $todo = $this->todo->find($id);
     return view('todo.show', ['todo' => $todo]);
 }
 
