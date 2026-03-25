3f0021403b6027e2babc8a14ccdcc7d69ea74580
tomtimtom000407-star <tomtimtom000407@gmail.com>
2026-03-25 19:29:54 +0900

    詳細画面表示機能作成

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,30 +8,32 @@ class TodoController extends Controller
 {
      public function index()
     {
-    
         $todo = new Todo();
-          $todos = $todo->all();
-           $todos;
-
+        $todos = $todo->all();
         return view('todo.index', ['todos' => $todos]);
     }
 
     
-public function create()
-{
-   return view('todo.create'); 
-}
+    public function create()
+    {
+        return view('todo.create'); 
+    }
 
-public function store(Request $request)
-{
-$inputs = $request->all(); // 変更
+    public function store(Request $request)
+    {
+        $inputs = $request->all(); 
+        $todo = new Todo();
+        // $todo->user_id = Auth::id();
+        $todo->fill($inputs);
+        $todo->save();
+        return redirect()->route('todo.index');
+    }
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+        return view('todo.show', ['todo' => $todo]);
+    }
 
-    $todo = new Todo();
-    $todo->user_id = Auth::id(); 
-    $todo->fill($inputs);
-    $todo->save();
-
-    return redirect()->route('todo.index');
-}
 }
 
