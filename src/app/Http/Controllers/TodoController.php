2ed981215deadc16f1d3dafbf051c0d550fffec3
tomtimtom000407-star <tomtimtom000407@gmail.com>
2026-03-28 19:30:10 +0900

    編集機能の作成

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -42,5 +42,21 @@ class TodoController extends Controller
         return view('todo.show', ['todo' => $todo]);
 
     }
+    public function edit($id)
+    {
+    // TODO: 編集対象のレコードの情報を持つTodoモデルのインスタンスを取得
+        $todo = $this->todo->find($id);
+        return view('todo.edit', ['todo' => $todo]);
+    }
+    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    {
+    // TODO: リクエストされた値を取得
+        $inputs = $request->all();
+         // TODO: 更新対象のデータを取得
+        $todo = $this->todo->find($id);
+    // TODO: 更新したい値の代入とUPDATE文の実行
+        $todo->fill($inputs)->save();
+        return redirect()->route('todo.show', $todo->id);
+    }
 }
 
