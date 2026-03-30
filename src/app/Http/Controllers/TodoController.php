b9fcdc2043ef230ef01950b1a932c56a8a35bd4e
Haruka Shimura <y29814021@gmail.com>
2026-03-31 01:42:31 +0900

    バリデーション実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 
 use App\Todo;
 
@@ -31,7 +31,7 @@ class TodoController extends Controller
     return view('todo.create');
 }
 
-public function store(Request $request)
+public function store(TodoRequest $request)
 {
     //フォームから送信された値を一括で取得
     $inputs = $request->all();
@@ -59,7 +59,7 @@ public function edit($id)
 }
 
 //リクエストされた値取得
-public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
 {
     // TODO: リクエストされた値を取得
     $inputs = $request->all();
