df9977f4572ae361c44a97dc988b4a194c58d8e9
tomtimtom000407-star <tomtimtom000407@gmail.com>
2026-04-04 12:04:24 +0900

    削除処理記載

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest; 
 use App\Todo;
 
 class TodoController extends Controller
@@ -28,7 +28,7 @@ class TodoController extends Controller
         return view('todo.create'); 
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all(); 
         $this->todo->fill($inputs); 
@@ -48,15 +48,23 @@ class TodoController extends Controller
         $todo = $this->todo->find($id);
         return view('todo.edit', ['todo' => $todo]);
     }
-    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
     {
     // TODO: リクエストされた値を取得
         $inputs = $request->all();
          // TODO: 更新対象のデータを取得
         $todo = $this->todo->find($id);
     // TODO: 更新したい値の代入とUPDATE文の実行
-        $todo->fill($inputs)->save();
+        $todo->fill($inputs);
+        $todo->save();
         return redirect()->route('todo.show', $todo->id);
+    }
+
+    public function delete($id)
+    {
+        $todo = $this->todo->find($id);
+        $todo->delete();
+        return redirect()->route('todo.index');
     }
 }
 
