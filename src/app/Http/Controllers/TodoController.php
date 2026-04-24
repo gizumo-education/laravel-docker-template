75df71ac4a6425e3c5db6b26a6aeb018e5586afd
skybluequartet7931-gif <skybluequartet7931@gmal.com>
2026-04-24 14:36:18 +0900

    feat: ToDo新規作成画面を表示する機能を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -12,7 +12,14 @@ class TodoController extends Controller
         $todo = new Todo();
         $todos = $todo->all();
         
-        return view('todo.index', ['todos' => $todos]);
+        return view('todo.index',  ['todos' => $todos]);
         
     }
+
+    public function create()
+    {
+        // TODO: 第1引数を指定
+        return view('todo.create');
+    }
+
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 // });
 
 Route::get('/todo', 'TodoController@index');
+Route::get('/todo/create', 'TodoController@create');
