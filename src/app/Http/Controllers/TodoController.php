324fbfc55efc98c94f9e755ac8047cf62dd1d607
skybluequartet7931-gif <skybluequartet7931@gmal.com>
2026-04-24 11:38:28 +0900

    feat: 取得したTodoデータを一覧ビューに渡す処理を追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -12,6 +12,7 @@ class TodoController extends Controller
         $todo = new Todo();
         $todos = $todo->all();
         
-        return view('todo.index');
+        return view('todo.index', ['todos' => $todos]);
+        
     }
 }
