2acd18b4d0bf6c6417dc7b4e2e5cc2775f8f26e0
skybluequartet7931-gif <skybluequartet7931@gmal.com>
2026-04-27 18:29:42 +0900

    feat: ToDo新規作成機能の実装（DB保存処理の追加）

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use App\Todo;
 
 class TodoController extends Controller
@@ -22,4 +22,17 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
+    public function store(Request $request)
+{
+     $content = $request->input('content');
+
+     // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+    $todo = new Todo(); 
+    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+    $todo->content = $content;
+    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+    $todo->save();
+
+    return redirect()->route('todo.index');
+}
 }
