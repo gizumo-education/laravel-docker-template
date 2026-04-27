b7a352f16e8213a770b26310050b95d9b6164246
skybluequartet7931-gif <skybluequartet7931@gmal.com>
2026-04-27 17:10:54 +0900

    refactor: 一覧と新規作成画面の共通部分をコンポーネント化

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,4 +20,4 @@ Route::get('/', function () {
 // });
 
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create');
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
