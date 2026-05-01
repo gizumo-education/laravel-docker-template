e1d2c8c4b6f4ef8b1b1c55c65940689e4e4e761e
skybluequartet7931-gif <skybluequartet7931@gmal.com>
2026-05-01 16:24:33 +0900

    refactor: ToDo削除機能を論理削除に変更

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // 追記 Laravel②
 
 class Todo extends Model
 {
+   use SoftDeletes; // 追記 Laravel②
+
    protected $table = 'todos';
 
    protected $fillable = [
