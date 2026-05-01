b66e20745077a70b2df13d462f03d0c813286450
skybluequartet7931-gif <skybluequartet7931@gmal.com>
2026-05-01 10:30:29 +0900

    refactor: Todoインスタンス化処理の共通化

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,10 +7,14 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo; //Lalavel②
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        // $todo = new Todo();
+        // $todos = $todo->all();
+        // 以下に変更  Laravel②
+        $todos = $this->todo->all();
         
         return view('todo.index',  ['todos' => $todos]);
         
@@ -25,9 +29,11 @@ class TodoController extends Controller
 {
     $inputs = $request->all();
 
-    $todo = new Todo(); 
-    $todo->fill($inputs);
-    $todo->save();
+    // $todo = new Todo(); 
+    // $todo->fill($inputs);
+    // $todo->save();
+    $this->todo->fill($inputs); // 変更 Laravel②
+    $this->todo->save(); // 変更 Laravel②
 
     return redirect()->route('todo.index');
 }
@@ -35,9 +41,16 @@ class TodoController extends Controller
 // Lalavel②
 public function show($id)
 {
-    $model = new Todo();
-    $todo = $model->find($id);
+    // $model = new Todo();
+    // $todo = $model->find($id);
+    // 以下に変更
+    $todo = $this->todo->find($id);
 
     return view('todo.show', ['todo' => $todo]);
 }
+
+public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
 }
