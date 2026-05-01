58640acd092f34608792ff3eb2b8707c4cad5755
skybluequartet7931-gif <skybluequartet7931@gmal.com>
2026-05-01 12:28:39 +0900

    feat: ToDoの更新処理（Update）を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -26,7 +26,7 @@ class TodoController extends Controller
     }
 
     public function store(Request $request)
-{
+    {
     $inputs = $request->all();
 
     // $todo = new Todo(); 
@@ -36,32 +36,45 @@ class TodoController extends Controller
     $this->todo->save(); // 変更 Laravel②
 
     return redirect()->route('todo.index');
-}
+    }
 
 // Lalavel②
-public function show($id)
-{
+    public function show($id)
+    {
     // $model = new Todo();
     // $todo = $model->find($id);
     // 以下に変更
     $todo = $this->todo->find($id);
 
     return view('todo.show', ['todo' => $todo]);
-}
+    }
 
-public function __construct(Todo $todo)
+    public function __construct(Todo $todo)
     {
         $this->todo = $todo;
     }
 
 // TODO: ルートパラメータを引数に受け取る
-public function edit($id)
+    public function edit($id)
     {
     // TODO: 編集対象のレコードの情報を持つTodoモデルのインスタンスを取得
         $todo = $this->todo->find($id);
 
         // TODO: view()を使用して編集画面を表示
         return view('todo.edit', ['todo' => $todo]);
+    }
+
+    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    {
+    // TODO: リクエストされた値を取得
+        $inputs = $request->all();
+
+    // TODO: 更新対象のデータを取得
+        $todo = $this->todo->find($id);
+    // TODO: 更新したい値の代入とUPDATE文の実行
+        $todo-> fill($inputs)->save();
+
+         return redirect()->route('todo.show', $todo->id); // 追記
     }
 
 }
