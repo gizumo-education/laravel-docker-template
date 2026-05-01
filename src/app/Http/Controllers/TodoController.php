a09482279a507d9336fa24b87b560c4e1c9b3cb3
skybluequartet7931-gif <skybluequartet7931@gmal.com>
2026-05-01 11:41:54 +0900

    feat: ToDo編集画面の表示機能を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -53,4 +53,15 @@ public function __construct(Todo $todo)
     {
         $this->todo = $todo;
     }
+
+// TODO: ルートパラメータを引数に受け取る
+public function edit($id)
+    {
+    // TODO: 編集対象のレコードの情報を持つTodoモデルのインスタンスを取得
+        $todo = $this->todo->find($id);
+
+        // TODO: view()を使用して編集画面を表示
+        return view('todo.edit', ['todo' => $todo]);
+    }
+
 }
