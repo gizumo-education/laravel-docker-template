d4bf9c67c4b4221eb722d08eb36eec0be3193107
itsuki0829 <itsuki08290829@gmail.com>
2026-05-04 21:11:08 +0900

    add: todo更新機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -43,10 +43,20 @@ class TodoController extends Controller
     }
     
     public function edit($id)
-{
+    {
     $todo = $this->todo->find($id);
 
     return view('todo.edit', ['todo' => $todo]);
-}
+    }
+
+    public function update(Request $request, $id)
+    {
+    $inputs = $request->all();
+    $todo = $this->todo->find($id);
+    $todo->fill($inputs)->save();
+
+    return redirect()->route('todo.show', $todo->id);
+
+    }
 
 }
--- src/resources/views/todo/edit.blade.php
+++ src/resources/views/todo/edit.blade.php
@@ -5,19 +5,20 @@
     <div class="card">
       <div class="card-header">ToDo編集</div>
       <div class="card-body">
-        <form method="POST" action="">
-          @csrf
-          <div class="form-group row">
+        <form method="POST" action="{{ route('todo.update', $todo->id) }}">
+        @csrf
+        @method('PUT')
+        <div class="form-group row">
             <label for="name" class="col-md-4 col-form-label text-md-right">ToDo入力</label>
             <div class="col-md-6">
-              <input type="text" class="form-control" name="content" value="{{ $todo->content }}">
+            <input type="text" class="form-control" name="content" value="{{ $todo->content }}">
             </div>
-          </div>
-          <div class="form-group row mb-0">
+        </div>
+        <div class="form-group row mb-0">
             <div class="col-md-6 offset-md-4">
-              <button type="submit" class="btn btn-primary">更新</button>
+            <button type="submit" class="btn btn-primary">更新</button>
             </div>
-          </div>
+        </div>
         </form>
       </div>
     </div>
