c73e8d986990a7d4f74ea33b1a1f48c5bfe28de1
itsuki0829 <itsuki08290829@gmail.com>
2026-05-04 20:06:06 +0900

    refactor: TodoControllerのモデルインスタンスを共通化

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,12 +7,13 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo;
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
-        
-        return view('todo.index', ['todos' => $todos]);
+    $todos = $this->todo->all();
+
+    return view('todo.index', ['todos' => $todos]);
     }
 
     public function create()
@@ -23,20 +24,22 @@ class TodoController extends Controller
     public function store(Request $request)
     {
     $inputs = $request->all();
-    $todo = new Todo();
-    $todo->content = $inputs['content'];
-    // $todo->fill($inputs);
 
-    $todo->save();
+    $this->todo->fill($inputs); 
+    $this->todo->save(); 
+
     return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-       $model = new Todo();
-       $todo = $model->find($id);
-       
+       $todo = $this->todo->find($id);
        return view('todo.show', ['todo' => $todo]);
+    }
+
+        public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
     }
 
 
