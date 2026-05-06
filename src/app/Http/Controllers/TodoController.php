c33c9263540be246bdc98b8454a0793e4e605f46
itsuki0829 <itsuki08290829@gmail.com>
2026-05-06 18:15:00 +0900

    add: todo削除機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -59,4 +59,13 @@ class TodoController extends Controller
 
     }
 
+    public function delete($id)
+{
+    $todo = $this->todo->find($id);
+
+    $todo->delete();
+
+    return redirect()->route('todo.index');
+}
+
 }
