fa16d900a2f686835f97325ad7dd8cee64a98a52
matecha <miyashiromomoe@gmail.com>
2026-05-07 18:09:53 +0900

    feat: 取得したデータを画面に渡す処理を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -13,6 +13,6 @@ class TodoController extends Controller
         $todo = new Todo();
         $todos = $todo->all();
 
-        return view('todo.index');
+        return view('todo.index', ['helloWorld' => 'hello World!']);
     }
 }
