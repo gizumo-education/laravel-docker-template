021a8bed89517d5a643cc68c337bdd9ebe7cdb51
otoka watanabe <watanabe.otoka@gmail.com>
2026-07-29 17:39:48 +0900

    feat:DBに登録データを取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     //
         public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+        // dd($todos);
+
         return view('todo.index'); 
     }
 }
