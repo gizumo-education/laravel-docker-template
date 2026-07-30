b05e009d7923aa9bf223050e930c85fefe8ad749
otoka watanabe <watanabe.otoka@gmail.com>
2026-07-30 12:27:54 +0900

    feat:ToDo新規作成機能の実装一括代入

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -22,13 +22,14 @@ class TodoController extends Controller
 
 public function store(Request $request)
 {
-     $content = $request->input('content'); // 追記
+     $inputs = $request->all(); // 追記
+     dd($inputs); 
 
     // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
     $todo = new Todo(); 
+    $todo->user_id = Auth::id();
     // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-    $todo->content = $content;
-    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+    $todo->fill($inputs);     // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
     $todo->save();
 
     return redirect()->route('todo.index');
