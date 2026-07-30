7ec726b8ac433f150dd719eeb97b33cff9920112
otoka watanabe <watanabe.otoka@gmail.com>
2026-07-30 12:11:45 +0900

    feat:DBにデータを保存

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,5 +11,7 @@
 |
 */
 
+Route::post('/todo', 'TodoController@store')->name('todo.store');
 Route::get('/todo', 'TodoController@index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create'); 
+Route::get('/todo', 'TodoController@index')->name('todo.index'); // ルート名の定義を追記
