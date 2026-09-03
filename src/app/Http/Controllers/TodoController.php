8a264a50893ef6f26efe73e04be3b846bf551c31
廣瀬溫菜 <hirose@hirosenoMacBook-Pro.local>
2026-09-03 11:50:28 +0900

    refactor:controllerのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -1,42 +1,42 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Todo;
+use Illuminate\Http\Request;
 
 class TodoController extends Controller
-{
+{   
+    private $todo;
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('todo.index', ['todos' => $todos]);
 
     }
     public function create()
-{
-    // dd('新規作成画面のルート実行！');
-    return view('todo.create');
+    {
+        // dd('新規作成画面のルート実行！');
+        return view('todo.create');
+    }
+
+    public function store(Request $request)
+    {
+        $inputs = $request->all();
+
+       $this->todo->fill($inputs);
+         $this->todo->save();
+
+        return redirect()->route('todo.index');
+    }
+    public function show($id)
+    {
+        $todo = $this->todo->find($id);
+        return view('todo.show', ['todo' => $todo]);
+
+    }
 }
-
-public function store(Request $request)
-
-{
-    $inputs = $request->all();
-    dd($inputs);
-    
-    // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-    $todo = new Todo(); 
-    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-    $todo->fill($inputs);
-    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-    $todo->save();
-
-    return redirect()->route('todo.index');
-}
-
-}
-
-
